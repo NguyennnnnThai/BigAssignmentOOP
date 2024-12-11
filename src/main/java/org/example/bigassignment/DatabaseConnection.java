e6f1f02269aff1b13ebadd4dbd5695f02316ac33
NguyennnnnThai <thainguyenzeno@gmail.com>
2024-12-11 15:42:29 +0700

    test

--- src/main/java/org/example/bigassignment/DatabaseConnection.java
+++ src/main/java/org/example/bigassignment/DatabaseConnection.java
@@ -5,7 +5,7 @@
     import java.sql.SQLException;
 
     public class DatabaseConnection {
-        private static final String URL = "jdbc:mysql://localhost:3306/library";
+        private static final String URL = "jdbc:mysql://localhost:3306/library_system";
         private static final String USER = "root"; // Thay đổi nếu cần
         private static final String PASSWORD = "1234"; // Thay đổi nếu cần
 
